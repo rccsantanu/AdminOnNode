b96b98cdd231f314c078518f061cbc9da05db236
basantshaw7690 <basant.webdev@gmail.com>
2020-07-11 17:47:25 +0530

    added files

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/nodeAdmindb',{
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://basant_admin:kv5dSIA4UK51AzRp@cluster0-ipolq.mongodb.net/AdminOnNode?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-},(err)=>{
-    if(err){
-        console.log('Error in db connection :' + err);
-    }else{
-        console.log("Mongo: Database connected successfully");
+  },
+  err => {
+    if (err) {
+      console.log("Error in db connection :" + err);
+    } else {
+      console.log("Mongo: Database connected successfully");
     }
-});
-
+  }
+);
